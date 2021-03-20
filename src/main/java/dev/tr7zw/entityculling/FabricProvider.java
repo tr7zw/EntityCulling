0e7e640b38d2dce7c44e476771d8e2fd0b58ca3e
tr7zw <tr7zw@live.de>
2021-03-20 23:21:15 +0100

    Add debug

--- src/main/java/dev/tr7zw/entityculling/FabricProvider.java
+++ src/main/java/dev/tr7zw/entityculling/FabricProvider.java
@@ -1,9 +1,11 @@
 package dev.tr7zw.entityculling;
 
 import com.logisticscraft.occlusionculling.DataProvider;
+import com.logisticscraft.occlusionculling.util.Vec3d;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.math.BlockPos;
 
 public class FabricProvider implements DataProvider {
@@ -26,6 +28,16 @@ public class FabricProvider implements DataProvider {
     @Override
     public void cleanup() {
         world = null;
+    }
+
+    @Override
+    public void checkingPosition(Vec3d[] targets, int targetSize, Vec3d viewerPosition) {
+        if(!EntityCullingMod.instance.debugHitboxes)return;
+        for (int i = 0; i < targetSize; i++) {
+            Vec3d target = targets[i];
+            client.world.addImportantParticle(ParticleTypes.HAPPY_VILLAGER, true, ((int) viewerPosition.x) + target.x,
+                    ((int) viewerPosition.y) + target.y, ((int) viewerPosition.z) + target.z, 0, 0, 0);
+        }
     }
 
 }
