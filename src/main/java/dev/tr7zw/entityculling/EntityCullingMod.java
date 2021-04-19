686e423ac5d63c42244300de173e46ba220f5e0a
tr7zw <tr7zw@live.de>
2021-04-19 23:23:18 +0200

    Don't crash on a broken config

--- src/main/java/dev/tr7zw/entityculling/EntityCullingMod.java
+++ src/main/java/dev/tr7zw/entityculling/EntityCullingMod.java
@@ -54,7 +54,8 @@ public class EntityCullingMod implements ModInitializer {
             try {
                 config = gson.fromJson(new String(Files.readAllBytes(settingsFile.toPath()), StandardCharsets.UTF_8),
                         Config.class);
-            } catch (IOException ex) {
+            } catch (Exception ex) {
+                System.out.println("Error while loading config! Creating a new one!");
                 ex.printStackTrace();
             }
         }
