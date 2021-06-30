2b401e07ac0a814b7d3f9be82e091709d887a1e2
tr7zw <tr7zw@live.de>
2021-06-30 11:45:15 +0200

    Add entity whitelist and stats

--- src/main/java/dev/tr7zw/entityculling/mixin/ClientWorldMixin.java
+++ src/main/java/dev/tr7zw/entityculling/mixin/ClientWorldMixin.java
@@ -5,6 +5,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import dev.tr7zw.entityculling.EntityCullingMod;
 import dev.tr7zw.entityculling.access.Cullable;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
@@ -17,19 +18,31 @@ public class ClientWorldMixin {
     
     @Inject(method = "tickEntity", at = @At("HEAD"), cancellable = true)
     public void tickEntity(Entity entity, CallbackInfo info) {
-        if(entity == mc.player || entity == mc.cameraEntity) {
+        if(!EntityCullingMod.instance.config.tickCulling) {
+            EntityCullingMod.instance.tickedEntities++;
+            return; // disabled
+        }
+        if(entity == mc.player || entity == mc.cameraEntity || entity == mc.player.getVehicle()) {
+            EntityCullingMod.instance.tickedEntities++;
             return; // never skip the client tick for the player itself
+        }
+        if(EntityCullingMod.instance.tickCullWhistelist.contains(entity.getType())) {
+            EntityCullingMod.instance.tickedEntities++;
+            return; // whitelisted, don't skip that tick
         }
         if(entity instanceof Cullable) {
             Cullable cull = (Cullable) entity;
             if(cull.isCulled() || cull.isOutOfCamera()) {
                 entity.resetPosition();
                 ++entity.age;
+                EntityCullingMod.instance.skippedEntityTicks++;
                 info.cancel();
+                return;
             } else {
                 cull.setOutOfCamera(true);
             }
         }
+        EntityCullingMod.instance.tickedEntities++;
     }
     
 }
