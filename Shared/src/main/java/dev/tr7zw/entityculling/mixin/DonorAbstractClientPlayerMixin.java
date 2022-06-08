b92814d0523204621ee392b9fe75c1bc04afdf39
tr7zw <tr7zw@live.de>
2022-06-08 22:49:06 +0200

    1.5.2, fix mod conflict

--- Shared/src/main/java/dev/tr7zw/entityculling/mixin/DonorAbstractClientPlayerMixin.java
+++ Shared/src/main/java/dev/tr7zw/entityculling/mixin/DonorAbstractClientPlayerMixin.java
@@ -16,24 +16,28 @@ import net.minecraft.resources.ResourceLocation;
 public class DonorAbstractClientPlayerMixin implements DonorProvider {
 
     public DonorSkinProvider trDonorProvider = null;
-    
+
     @Inject(method = "getSkinTextureLocation", at = @At("HEAD"), cancellable = true)
     public void getSkinTextureLocationDonor(CallbackInfoReturnable<ResourceLocation> ci) {
-        ResourceLocation loc = getDonorSkinProvider().getSkin();
-        if(loc != null) {
+        ResourceLocation loc = getAnimatedSkin();
+        if (loc != null) {
             ci.setReturnValue(loc);
-            ci.cancel(); 
+            ci.cancel();
         }
     }
 
+    /**
+     * This method ensures using the @Unique annotation that only one implementation
+     * runs. Not a perfect solution, hit me up if there is a better one
+     * 
+     * @return
+     */
     @Override
-    public DonorSkinProvider getDonorSkinProvider() {
-        if(trDonorProvider == null) {
-            trDonorProvider = new DonorSkinProvider(((AbstractClientPlayer)(Object)this).getUUID());
+    public ResourceLocation getAnimatedSkin() {
+        if (trDonorProvider == null) {
+            trDonorProvider = new DonorSkinProvider(((AbstractClientPlayer) (Object) this).getUUID());
         }
-        return trDonorProvider;
+        return trDonorProvider.getSkin();
     }
-    
 
-    
 }
