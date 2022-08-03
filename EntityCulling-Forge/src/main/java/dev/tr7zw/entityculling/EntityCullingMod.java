1f671110b0044aab7e6bfe7d8626401ef991c345
tr7zw <tr7zw@live.de>
2022-08-03 16:22:43 +0200

    Fix Forge 1.19.1
    Thic commit won't build due to https://github.com/architectury/architectury-loom/issues/96 , using "dev.architectury.loom" version "0.12.0.291" will build it.

--- EntityCulling-Forge/src/main/java/dev/tr7zw/entityculling/EntityCullingMod.java
+++ EntityCulling-Forge/src/main/java/dev/tr7zw/entityculling/EntityCullingMod.java
@@ -1,14 +1,14 @@
 package dev.tr7zw.entityculling;
 
-import net.minecraftforge.client.ClientRegistry;
-import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.ArrayUtils;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.phys.AABB;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent.ClientTickEvent;
-import net.minecraftforge.event.TickEvent.WorldTickEvent;
+import net.minecraftforge.event.TickEvent.LevelTickEvent;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -40,7 +40,7 @@ public class EntityCullingMod extends EntityCullingModBase {
 
     @Override
     public void initModloader() {
-        ClientRegistry.registerKeyBinding(keybind);
+        Minecraft.getInstance().options.keyMappings = ArrayUtils.add(Minecraft.getInstance().options.keyMappings, keybind);
         MinecraftForge.EVENT_BUS.addListener(this::doClientTick);
         MinecraftForge.EVENT_BUS.addListener(this::doWorldTick);
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
@@ -53,7 +53,7 @@ public class EntityCullingMod extends EntityCullingModBase {
         this.clientTick();
     }
     
-    private void doWorldTick(WorldTickEvent event) {
+    private void doWorldTick(LevelTickEvent event) {
         this.worldTick();
     }
 
