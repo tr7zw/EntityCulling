1faeddcb45f34f911a9aae741a2dcd6456f2092c
tr7zw <tr7zw@live.de>
2023-02-12 01:30:07 +0100

    Add more Botania projectiles to the default config. Fix #97

--- Shared/src/main/java/dev/tr7zw/entityculling/Config.java
+++ Shared/src/main/java/dev/tr7zw/entityculling/Config.java
@@ -8,7 +8,9 @@ public class Config {
 
     public int configVersion = 5;
     public boolean renderNametagsThroughWalls = true;
-    public Set<String> blockEntityWhitelist = new HashSet<>(Arrays.asList("minecraft:beacon", "create:rope_pulley", "create:hose_pulley", "betterend:eternal_pedestal"));
+    public Set<String> blockEntityWhitelist = new HashSet<>(
+            Arrays.asList("minecraft:beacon", "create:rope_pulley", "create:hose_pulley", "betterend:eternal_pedestal",
+                    "botania:magic_missile", "botania:flame_ring", "botania:falling_star"));
     public Set<String> entityWhitelist = new HashSet<>(Arrays.asList("botania:mana_burst"));
     public int tracingDistance = 128;
     public boolean debugMode = false;
@@ -16,7 +18,8 @@ public class Config {
     public int hitboxLimit = 50;
     public boolean skipMarkerArmorStands = true;
     public boolean tickCulling = true;
-    public Set<String> tickCullingWhitelist = new HashSet<>(Arrays.asList("minecraft:firework_rocket", "minecraft:boat"));
+    public Set<String> tickCullingWhitelist = new HashSet<>(
+            Arrays.asList("minecraft:firework_rocket", "minecraft:boat"));
     public boolean disableF3 = false;
-    
+
 }
