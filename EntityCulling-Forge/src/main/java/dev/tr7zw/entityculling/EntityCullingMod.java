8457115e1feda2ac59f1fae7140ff0c8a6c58161
tr7zw <tr7zw@live.de>
2023-02-17 23:26:27 +0100

    Bootstrap on Forge to prevent server crashes. Fix #100

--- EntityCulling-Forge/src/main/java/dev/tr7zw/entityculling/EntityCullingMod.java
+++ EntityCulling-Forge/src/main/java/dev/tr7zw/entityculling/EntityCullingMod.java
@@ -15,25 +15,14 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod("entityculling")
-public class EntityCullingMod extends EntityCullingModBase {
 
-    //Forge only
-    private boolean onServer = false;
+public class EntityCullingMod extends EntityCullingModBase {
     
     public EntityCullingMod() {
-        try {
-            Class clientClass = net.minecraft.client.Minecraft.class;
-        }catch(Throwable ex) {
-            System.out.println("EntityCulling Mod installed on a Server. Going to sleep.");
-            onServer = true;
-            return;
-        }
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        if(onServer)return;
         onInitialize();
     }
 
