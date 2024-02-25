7631ff1686bd7ae14ccba570e93ecade28527f47
BenXII12 <BenXII12@gmail.com>
2024-02-25 23:07:57 +0300

    try to fix #126

---
+++ Shared/src/main/java/dev/tr7zw/entityculling/mixin/CameraMixin.java
@@ -0,0 +1,23 @@
+package dev.tr7zw.entityculling.mixin;
+
+import net.minecraft.client.Camera;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * <h1>Fixing Mojang calculation inaccuracies in Third-person mode</h1>
+ *
+ * <p>Description: When the camera is rotated more than ~85 degrees, the camera position goes into the block by thousandths.</p>
+ * <br>
+ * <p>Solution: Subtract from the distance of the camera from</p>
+ * @author BenXII12
+ */
+@Mixin(Camera.class)
+public class CameraMixin {
+    @Inject(at = @At("RETURN"), method = "getMaxZoom", cancellable = true)
+    private void getDetachedCameraMaxDistanceFix(double startingDistance, CallbackInfoReturnable<Double> cir) {
+        cir.setReturnValue(cir.getReturnValueD() - 0.04);
+    }
+}
--- Shared/src/main/java/dev/tr7zw/entityculling/mixin/DebugHudMixin.java
+++ Shared/src/main/java/dev/tr7zw/entityculling/mixin/DebugHudMixin.java
@@ -1,23 +1,25 @@
 package dev.tr7zw.entityculling.mixin;
 
-import java.util.List;
-
+import dev.tr7zw.entityculling.EntityCullingModBase;
+import net.minecraft.client.gui.components.DebugScreenOverlay;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import dev.tr7zw.entityculling.EntityCullingModBase;
-import net.minecraft.client.gui.components.DebugScreenOverlay;
+import java.util.List;
 
 @Mixin(DebugScreenOverlay.class)
 public class DebugHudMixin {
 
+    @Unique
     private int lastTickedEntities = 0;
+    @Unique
     private int lastSkippedEntityTicks = 0;
     
     @Inject(method = "getGameInformation", at = @At("RETURN"))
-    public List<String> getLeftText(CallbackInfoReturnable<List<String>> callback) {
+    public void getLeftText(CallbackInfoReturnable<List<String>> callback) {
         if(EntityCullingModBase.instance.tickedEntities != 0 || EntityCullingModBase.instance.skippedEntityTicks != 0) {
             lastTickedEntities = EntityCullingModBase.instance.tickedEntities;
             lastSkippedEntityTicks = EntityCullingModBase.instance.skippedEntityTicks;
@@ -25,7 +27,7 @@ public class DebugHudMixin {
             EntityCullingModBase.instance.skippedEntityTicks = 0;
         }
         if(EntityCullingModBase.instance.config.disableF3) {
-            return callback.getReturnValue();
+            return;
         }
         List<String> list = callback.getReturnValue();
         list.add("[Culling] Last pass: " + EntityCullingModBase.instance.cullTask.lastTime + "ms");
@@ -36,13 +38,11 @@ public class DebugHudMixin {
             list.add("[Culling] Rendered Entities: " + EntityCullingModBase.instance.renderedEntities + " Skipped: " + EntityCullingModBase.instance.skippedEntities);
             list.add("[Culling] Ticked Entities: " + lastTickedEntities + " Skipped: " + lastSkippedEntityTicks);
         }
-        
+
         EntityCullingModBase.instance.renderedBlockEntities = 0;
         EntityCullingModBase.instance.skippedBlockEntities = 0;
         EntityCullingModBase.instance.renderedEntities = 0;
         EntityCullingModBase.instance.skippedEntities = 0;
-
-        return list;
     }
     
 }
