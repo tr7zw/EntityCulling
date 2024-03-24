371ad14f388d7a17e451b206b1fa0c5350b8dd26
tr7zw <tr7zw@live.de>
2024-03-24 18:18:51 +0100

    Add drg_flares:drg_flares to the default config

--- EntityCulling-Versionless/src/main/java/dev/tr7zw/entityculling/versionless/Config.java
+++ EntityCulling-Versionless/src/main/java/dev/tr7zw/entityculling/versionless/Config.java
@@ -11,7 +11,7 @@ public class Config {
     public Set<String> blockEntityWhitelist = new HashSet<>(
             Arrays.asList("minecraft:beacon", "create:rope_pulley", "create:hose_pulley", "betterend:eternal_pedestal",
                     "botania:magic_missile", "botania:flame_ring", "botania:falling_star"));
-    public Set<String> entityWhitelist = new HashSet<>(Arrays.asList("botania:mana_burst"));
+    public Set<String> entityWhitelist = new HashSet<>(Arrays.asList("botania:mana_burst", "drg_flares:drg_flares"));
     public int tracingDistance = 128;
     public boolean debugMode = false;
     public int sleepDelay = 10;
@@ -21,7 +21,7 @@ public class Config {
     public Set<String> tickCullingWhitelist = new HashSet<>(
             Arrays.asList("minecraft:firework_rocket", "minecraft:boat", "create:carriage_contraption",
                     "create:contraption", "create:gantry_contraption", "create:stationary_contraption",
-                    "mts:builder_existing", "mts:builder_rendering", "mts:builder_seat"));
+                    "mts:builder_existing", "mts:builder_rendering", "mts:builder_seat", "drg_flares:drg_flares"));
     public boolean disableF3 = false;
     public boolean skipEntityCulling = false;
     public boolean skipBlockEntityCulling = false;
