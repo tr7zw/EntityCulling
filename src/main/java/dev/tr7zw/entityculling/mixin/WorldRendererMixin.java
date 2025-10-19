c35fa63c028de23de4a6555e72dc55310b167fde
tr7zw <tr7zw@live.de>
2025-10-19 14:45:43 +0200

    Bump version

--- src/main/java/dev/tr7zw/entityculling/mixin/WorldRendererMixin.java
+++ src/main/java/dev/tr7zw/entityculling/mixin/WorldRendererMixin.java
@@ -54,10 +54,11 @@ public class WorldRendererMixin {
                 if (entity instanceof LivingEntity living) {
                     var renderer = Minecraft.getInstance().getEntityRenderDispatcher().getRenderer(living);
                     double d = GeneralUtil.getCameraEntity().distanceToSqr(entity);
-                    if (renderer instanceof LivingEntityRendererAccessor accessor && accessor
-                            .invokeShouldShowName(living, d)) {
+                    if (renderer instanceof LivingEntityRendererAccessor accessor
+                            && accessor.invokeShouldShowName(living, d)) {
                         net.minecraft.network.chat.Component display;
-                        if (entity instanceof net.minecraft.client.entity.ClientAvatarEntity avatar && d < 100 && (display = avatar.belowNameDisplay()) != null) {
+                        if (entity instanceof net.minecraft.client.entity.ClientAvatarEntity avatar && d < 100
+                                && (display = avatar.belowNameDisplay()) != null) {
                             var avatarState = new net.minecraft.client.renderer.entity.state.AvatarRenderState();
                             avatarState.entityType = EntityType.PLAYER;
                             avatarState.scoreText = display;
