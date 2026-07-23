2c06e6ed224e8cf2fd865071a46300f493d30317
tr7zw <tr7zw@live.de>
2026-07-23 22:32:36 +0200

    Add documentation

--- src/main/java/dev/tr7zw/entityculling/BlockEntityRenderFabricExtension.java
+++ src/main/java/dev/tr7zw/entityculling/BlockEntityRenderFabricExtension.java
@@ -6,6 +6,7 @@ import net.minecraft.world.phys.*;
 
 /*
 Copy from NeoForge, allowing mods to have the same functionality as NeoForge's BlockEntityRenderExtension on Fabric.
+See https://github.com/tr7zw/EntityCulling/issues/313 / https://github.com/cc-tweaked/CC-Tweaked/commit/f2314178a5e5a3e6c0f6d7400c467ad12b1f6650 for more information and example usage.
  */
 public interface BlockEntityRenderFabricExtension<T extends BlockEntity> {
 
